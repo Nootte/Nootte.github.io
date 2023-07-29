1d8df88194772f12a1e7cc72502f318a08fbfc27
Sebastian Guerrero Manrique <sebasjuegos2003@hotmail.com>
2023-07-28 23:10:02 -0500

    Subir website
    He subido todos mis archivos

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+(function(){
+const sliders  =  [...document.querySelectorAll('.jugadores__body')];
+const buttonNext = document.querySelector('#next');
+const buttonBefore = document.querySelector('#before');
+let value;
+buttonNext.addEventListener('click',()=>{
+    changePosition(1);
+});
+
+buttonBefore.addEventListener('click',()=>{
+    changePosition(-1);
+});
+
+const changePosition = (add) =>{
+const currentJugadores = document.querySelector('.jugadores__body--show').dataset.id;
+value =Number(currentJugadores)
+value+=add;
+
+sliders[Number(currentJugadores)-1].classList.remove('jugadores__body--show')
+if(value === sliders.length+1|| value === 0 ){
+    value = value === 0? sliders.length : 1;
+
+}
+sliders[value-1].classList.add('jugadores__body--show');
+
+}
+
+})();
